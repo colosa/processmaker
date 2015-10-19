782ccd3aa9e77cc37c05247e47a01776e6eaa109
tomolimo <olivier.moron@araymond.com>
2015-10-19 13:22:42 +0200

    To allow several instances
    In order to allow several instances of PM on the same server

--- workflow/public_html/app.php
+++ workflow/public_html/app.php
@@ -7,6 +7,8 @@ if (isset($_SERVER['UNENCODED_URL'])) {
 }
 try {
     $rootDir = realpath(__DIR__ . "/../../") . DIRECTORY_SEPARATOR;
+    
+    session_name("pm_".md5($rootDir)) ;
 
     require $rootDir . "framework/src/Maveriks/Util/ClassLoader.php";
     $loader = Maveriks\Util\ClassLoader::getInstance();
